f05535ff86350817ed94c9efdf49137472e32249
blinno <business.kozlowskihubert@gmail.com>
2025-01-30 00:37:08 +0000

    points, dist between

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,21 @@ impl Rectangle {
 
 
 
+struct Point {
+    x: i32,
+    y: i32,
+}
+impl Point {
+    fn distance(&self, other: &Point) -> f64 {
+        // Pythagorean theorem
+        let x_squared = (self.x - other.x).pow(2); 
+        let y_squared = (self.y - other.y).pow(2); 
+        let pre_dist = (x_squared + y_squared) as f64;
+        let dist = pre_dist.sqrt();
+        dist
+    }
+}
+
 
 fn main() {
     let scale = 2;
@@ -26,4 +41,11 @@ fn main() {
     let rect2 = rect1.extend(scale);
     dbg!(&rect2);
     
+
+
+
+    let p1 = Point { x: 13, y: 45 };
+    let p2 = Point { x: -3, y: 18 };
+
+    println!("Distance between p1 and p2: {}", p1.distance(&p2));
 }
